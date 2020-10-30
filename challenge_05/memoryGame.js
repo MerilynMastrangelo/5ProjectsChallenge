ac718be0e06dbafefcb8ba51e3d2495f8800c7e9
Merilyn Mastrangelo <merilynlm@yahoo.com.br>
2020-10-30 18:52:30 +0000

    Disposal cards on board

--- challenge_05/memoryGame.js
+++ challenge_05/memoryGame.js
@@ -1,2 +1,30 @@
+const cards = document.querySelectorAll('.memory-card');
+const overlay = document.querySelectorAll('.overlay')
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
+
+cards.forEach(card => card.addEventListener('click', () => {
+    card.classList.toggle('flip');
+     
+    if(!hasFlippedCard){
+        // First Time Clicked
+        hasFlippedCard = true;
+        firstCard = card;
+
+    }else{
+        // Second Time Clicked
+        secondCard = card;
+        hasFlippedCard = false;
+
+        // Match?
+        if(firstCard.dataset.fruit === secondCard.dataset.fruit){
+            console.log('Matching Pieces!!!');
+            // alert('Matching Pieces!!!');
+        }else{
+            console.log("Pieces don't match! Sorry.");
+            // alert("Pieces don't match! Sorry.");
+        }
+    }
+}))
 
