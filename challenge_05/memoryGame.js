e12323f9e5efeeecce66e25106430f21fa1d2404
Merilyn Mastrangelo <merilynlm@yahoo.com.br>
2020-10-31 11:15:04 +0000

    matching pieces and solving double click

--- challenge_05/memoryGame.js
+++ challenge_05/memoryGame.js
@@ -1,30 +1,58 @@
 const cards = document.querySelectorAll('.memory-card');
-const overlay = document.querySelectorAll('.overlay')
+const scoreCount = document.querySelector('.score');
+const failedAttempt = document.querySelector('.failed-attempt')
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let score = 0;
+let failedCount = 0;
 
+function flipCard() {
+    if(this === firstCard) return;
+    this.classList.toggle('flip');
 
-cards.forEach(card => card.addEventListener('click', () => {
-    card.classList.toggle('flip');
-     
     if(!hasFlippedCard){
         // First Time Clicked
         hasFlippedCard = true;
-        firstCard = card;
-
+        firstCard = this;
+        console.log(hasFlippedCard, firstCard)
     }else{
         // Second Time Clicked
-        secondCard = card;
+        secondCard = this;
         hasFlippedCard = false;
 
         // Match?
         if(firstCard.dataset.fruit === secondCard.dataset.fruit){
+            disabledCards();
             console.log('Matching Pieces!!!');
-            // alert('Matching Pieces!!!');
-        }else{
-            console.log("Pieces don't match! Sorry.");
-            // alert("Pieces don't match! Sorry.");
+            score++;
+            scoreCount.innerText = score;
+            if(score >= 3){
+                alert('You won!');
+            }
         }
-    }
-}))
+        else{
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+                resetBoard();
+            },500);
+            failedCount++;
+            failedAttempt.innerText = failedCount;
+        }
+}
+}
 
+function disabledCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+
+function resetBoard() {
+    hasFlippedCard = false;
+    // Reseting cards
+    firstCard = null;
+    secondCard = null;
+}
+cards.forEach(card => card.addEventListener('click', flipCard));
