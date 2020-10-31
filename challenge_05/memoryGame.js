b40991adb1930dca1e3531a5067819726200829b
Merilyn Mastrangelo <merilynlm@yahoo.com.br>
2020-10-31 11:44:06 +0000

    last project complete

--- challenge_05/memoryGame.js
+++ challenge_05/memoryGame.js
@@ -1,20 +1,20 @@
 const cards = document.querySelectorAll('.memory-card');
 const scoreCount = document.querySelector('.score');
-const failedAttempt = document.querySelector('.failed-attempt')
+const failedAttempt = document.querySelector('.failed-attempt');
+
 let hasFlippedCard = false;
 let firstCard, secondCard;
-let score = 0;
-let failedCount = 0;
+let score = 0, failedCount = 0;
 
 function flipCard() {
     if(this === firstCard) return;
+    // Add and remove a class to see the front face
     this.classList.toggle('flip');
 
     if(!hasFlippedCard){
         // First Time Clicked
         hasFlippedCard = true;
         firstCard = this;
-        console.log(hasFlippedCard, firstCard)
     }else{
         // Second Time Clicked
         secondCard = this;
@@ -22,37 +22,49 @@ function flipCard() {
 
         // Match?
         if(firstCard.dataset.fruit === secondCard.dataset.fruit){
-            disabledCards();
-            console.log('Matching Pieces!!!');
-            score++;
-            scoreCount.innerText = score;
-            if(score >= 3){
-                alert('You won!');
-            }
+            matchingCards();
         }
         else{
-            setTimeout(() => {
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-                resetBoard();
-            },500);
-            failedCount++;
-            failedAttempt.innerText = failedCount;
+            unflipingCards();
         }
 }
 }
 
 function disabledCards(){
+    // Prevent the card can't eb clicked again
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
     resetBoard();
 }
 
+function matchingCards(){
+    disabledCards();
+    // Counting scores
+    score++;
+    scoreCount.innerText = score;
+    // Won message
+    if(score >= 3){
+        alert('You won!');
+    }
+}
+
+function unflipingCards(){
+    // We remove the flip class means that card returns to back face after 0.5s
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    },500);
+    // Counting failed Attempt
+    failedCount++;
+    failedAttempt.innerText = failedCount;
+}
 
 function resetBoard() {
     hasFlippedCard = false;
-    // Reseting cards
+    // Reseting both cards preventing double clicks in same card
     firstCard = null;
     secondCard = null;
 }
+
 cards.forEach(card => card.addEventListener('click', flipCard));
