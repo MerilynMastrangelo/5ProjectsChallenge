046dc5fedb57a08b2854539cd4667b7f226d37aa
Merilyn Mastrangelo <merilynlm@yahoo.com.br>
2020-11-15 17:23:54 +0000

    added delete all tasks

--- challenge_01/todo-list-oop.js
+++ challenge_01/todo-list-oop.js
@@ -1,30 +1,30 @@
 // Task
-class Task{
+class Task {
     static counter = 0;
-    constructor(name){
+    constructor(name) {
         this.name = name;
     }
-    static tasksRemaining(){
+    static tasksRemaining() {
         const h4 = document.querySelector('h4');
 
-        if(Task.counter === 0){
-            {h4.innerHTML = `No Tasks Remaining`;}
-            
-            document.querySelector('#select').className = "list-group-item mx-auto w-50 invisible";
-        }else{
-            
+        if (Task.counter === 0) {
+            { h4.innerHTML = `No Tasks Remaining`; }
+
+            // document.querySelector('#select').className = "list-group-item mx-auto w-50 visible";
+        } else {
+
             h4.innerHTML = `${Task.counter} Tasks Remaining`;
-            document.querySelector('#select').className = "list-group-item mx-auto w-50 visible";
+            // document.querySelector('#select').className = "list-group-item mx-auto w-50 visible";
         }
     }
 }
 
-class Ui{
-    static addTaskToList(task){
-        if(Task.counter > 0){
+class Ui {
+    static addTaskToList(task) {
+        if (Task.counter > 0) {
             document.querySelector('#result').className = "result mt-5 visible";
             const unorderedList = document.querySelector('ul');
-            
+
             unorderedList.innerHTML += `
             <li class="list-group-item mx-auto w-50">
             <div class="d-inline d-flex justify-content-between">
@@ -41,16 +41,17 @@ class Ui{
             `;
 
             document.querySelector('#result').appendChild(unorderedList);
+        }
+
     }
-        
-    }
-    static clearFields(){
+    static clearFields() {
         document.querySelector('form').reset();
     }
-    static deleteTask(el){
-        if(el.classList.contains('delete')){
+    static deleteTask(el) {
+        if (el.classList.contains('delete')) {
             Task.counter--;
-            el.parentElement.parentElement.parentElement.remove();
+            const rmEl = el.parentElement.parentElement.parentElement;
+            rmEl.remove();
         }
     }
     
@@ -66,9 +67,9 @@ document.querySelector('form').addEventListener('submit', (e) => {
     const enteredTask = document.querySelector('#inputTask').value;
 
     // Validate
-    if(enteredTask === ''){
+    if (enteredTask === '') {
         Ui.showAlert('PLease fill in all fields', 'danger');
-    }else{
+    } else {
         // Instanciate a Task
         const newTask = new Task(enteredTask);
 
@@ -79,8 +80,22 @@ document.querySelector('form').addEventListener('submit', (e) => {
 
         // Clear fields
         Ui.clearFields();
+
+        document.querySelector('.trash').addEventListener('click', () => {
+            let boxes = document.querySelectorAll('#taskAdded');
+
+            
+            boxes.forEach((box) => {
+                const newVar = box.parentElement.parentElement.parentElement;
+                newVar.remove();
+                
+            })
+          
+        })
+
     }
 });
+
 
 document.querySelector('ul').addEventListener('click', (e) => {
     Ui.deleteTask(e.target);
